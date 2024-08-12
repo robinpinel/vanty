28eef8305243a48740ad6c97978f450f2cd1e6de
robin <robin.pinel@gmail.com>
2024-08-12 13:34:02 +0200

    Probar mensajes

--- js/contact_me.js
+++ js/contact_me.js
@@ -68,3 +68,22 @@ $('#name').focus(function() {
     $('#success').html('');
 });
 */
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("./mail/contact_me.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("Thank you for your submission"))
+      .catch((error) => alert(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
