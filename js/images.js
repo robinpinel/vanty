9f0b47326c1cc56cf30eb8d6cb2d0858855162b4
robin <robin.pinel@gmail.com>
2024-08-18 19:31:16 +0200

    Nuevas Categorias

---
+++ js/images.js
@@ -0,0 +1,140 @@
+const imagesG = [
+    {src: "img/portfolio/CUPULAS 2.jpg", title: "Cúpulas", description: "Gouache. 150 X 100"},
+    {src: "img/portfolio/El Mirador.jpg", title: "El Mirador", description: "Gouache. 120 X 80"},
+    {src: "img/portfolio/EL OTRO LADO.jpg", title: "El otro lado", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/LA BAILARINA DE LA CAVERNA.jpg", title: "La bailarina de la caverna", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/La Rueda 2.jpg", title: "La Rueda", description: "Gouache. 100 X 80"},
+    {src: "img/portfolio/LAS RUEDAS 2.jpg", title: "Las Ruedas", description: "Acrílico y Gouache. 120 X 80"},
+    {src: "img/portfolio/Las zanjas.jpg", title: "Las zanjas", description: "Gouache. 100 X 80"},
+    {src: "img/portfolio/OBELISCOS.jpg", title: "Obeliscos", description: "Gouache. 100 X 80"},
+    {src: "img/portfolio/PUESTA DE SOL.jpg", title: "Puesta de sol", description: "Gouache. 100 X 50"},
+    {src: "img/portfolio/MARTE INVADIDO.jpg", title: "Marte Invadido", description: "Gouache. 100 X 40"},
+    {src: "img/portfolio/MEZCLOPOLIS.jpg", title: "Mezclopolis", description: "Gouache. 100 X 50"},
+    {src: "img/portfolio/PLAZA DE ROTOS.jpg", title: "Plaza de Rotos", description: "Gouache. 100 X 50"},
+    {src: "img/portfolio/VAGAMUNDOS.jpg", title: "Vagamundos", description: "Gouache. 120 X 80"},
+    {src: "img/portfolio/PERDIDOS EN EL TIEMPO.jpg", title: "Perdidos en el tiempo", description: "Acrílico y Gouache. 120 X 80"},
+    {src: "img/portfolio/MARATHON DE LOS RELOJES.jpg", title: "Marathon de relojes", description: "Acrílico y Gouache. 120 X 80"},
+    {src: "img/portfolio/PAIS DE LAS CÚPULAS.jpg", title: "País de las Cúpulas", description: "(Adquirido)"},
+    {src: "img/portfolio/Obeliscos II.jpg", title: "Obeliscos II", description: "Gouache. 80 X 60"},
+    {src: "img/portfolio/LA BANDA DE LA SABANA.jpg", title: "La banda de la Sabana", description: "Gouache. 120 X 50"},
+    {src: "img/portfolio/JAZZ NATURAL.jpg", title: "Jazz Natural", description: "Gouache. 220 X 80"},
+    {src: "img/portfolio/El Niño 400.jpg", title: "El Niño", description: ""},
+    {src: "img/portfolio/LA DANZA DE LAS BRUJAS.jpg", title: "La danza de las brujas", description: "Gouache. 120 X 80"},
+    {src: "img/portfolio/MEIGAS.jpg", title: "Meigas", description: "Gouache. 100 X 50"},
+    {src: "img/portfolio/El salto Universal.jpg", title: "El salto Universal", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/El Jardín de las Cúpulas.jpg", title: "El Jardín de las Cúpulas", description: "Oleo. 120 X 80"},
+    {src: "img/portfolio/EL BALCÓN.jpg", title: "El balcón", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/LA RED.jpg", title: "La Red", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/LA TORRE DEL OJO.jpg", title: "La torre del ojo", description: "Gouache. 100 X 40"},
+    {src: "img/portfolio/TEMPLARIOS.jpg", title: "Templarios", description: ""},
+    {src: "img/portfolio/Los monjes.jpg", title: "Los monjes", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/Los monjes 2.jpg", title: "Los monjes 2", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/SOSIEGO 750.jpg", title: "Sosiego", description: ""},
+    {src: "img/portfolio/Alivio.jpg", title: "Alivio", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/LOS VINILOS 450.jpg", title: "Los Vinilos", description: ""},
+    {src: "img/portfolio/TANGO.jpg", title: "Tango", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/CAMINANTES.jpg", title: "Caminantes", description: "Acrílico y Gouache. 100 X 40"},
+    {src: "img/portfolio/PLACIDED 500.jpg", title: "Placided", description: ""},
+    {src: "img/portfolio/PERCUSIÓN.jpg", title: "Percusión", description: "Gouache. 100 X 80"},
+    {src: "img/portfolio/EL TROMPETA.jpg", title: "El Trompeta", description: "Gouache. 100 X 60"},
+    {src: "img/portfolio/Ray Charles.jpg", title: "Ray Charles", description: "Gouache. 100 X 60"}
+
+ ];
+
+ const imagesA = [
+    {src: "img/portfolio/STONEHENGE.jpg", title: "Stonehenge", description: "Acrílico. 90 X 40"},
+    {src: "img/portfolio/LA CALMA.jpg", title: "Calma", description: "Acrílico. 100 X 50"},
+    {src: "img/portfolio/TAJ MAHAL.jpg", title: "Taj Mahal", description: "Acrílico. 80 X 60"},
+    {src: "img/portfolio/Macky.jpg", title: "Macky", description: "Acrílico. 100 X 60"}
+ ];
+
+ const imagesO = [
+    {src: "img/portfolio/contraluz.jpg", title: "Contraluz", description: "Oleo. 100 X 50"},
+    {src: "img/portfolio/ORIENTE.jpg", title: "Oriente", description: "Oleo. 90 X 40"},
+    {src: "img/portfolio/CAVERNIMENTOS 2.jpg", title: "Cavernimientos", description: "Oleo. 100 X 60"},
+    {src: "img/portfolio/De algo hay que morir.jpg", title: "De algo hay que morir", description: "Oleo. 100 X 60"},
+    {src: "img/portfolio/Arquitectura.jpg", title: "Arquitectura", description: "Oleo. 100 X 60"},
+    {src: "img/portfolio/Colores.jpg", title: "Colores", description: "Oleo. 120 X 80"}
+ ];
+ const imagesCarb = [
+    {src: "img/portfolio/carb1.jpg", title: "", description: ""},
+    {src: "img/portfolio/carb2.jpg", title: "", description: ""},
+    {src: "img/portfolio/carb3.jpg", title: "", description: ""},
+    {src: "img/portfolio/carb4.jpg", title: "", description: ""},
+    {src: "img/portfolio/carb5.jpg", title: "", description: ""},
+    {src: "img/portfolio/carb6.jpg", title: "", description: ""}
+ ];
+
+ function generateGallery() {
+    const galleryContainer = document.querySelector('.portfolio-items');
+    imagesG.forEach(image => {
+       const item = document.createElement('div');
+       item.className = 'col-sm-6 col-md-3 col-lg-3 Gouache';
+       item.innerHTML = `
+          <div class="portfolio-item">
+             <div class="hover-bg">
+                <a href="${image.src}" title="${image.title}" data-description="${image.description}" data-lightbox-gallery="gallery1">
+                   <div class="hover-text">
+                      <h4>${image.title}</h4>
+                   </div>
+                   <img src="${image.src}" class="img-responsive" alt="${image.title}">
+                </a>
+             </div>
+          </div>`;
+       galleryContainer.appendChild(item);
+    });
+
+    imagesA.forEach(image => {
+        const item = document.createElement('div');
+        item.className = 'col-sm-6 col-md-3 col-lg-3 Acrílico';
+        item.innerHTML = `
+           <div class="portfolio-item">
+              <div class="hover-bg">
+                 <a href="${image.src}" title="${image.title}" data-description="${image.description}" data-lightbox-gallery="gallery1">
+                    <div class="hover-text">
+                       <h4>${image.title}</h4>
+                    </div>
+                    <img src="${image.src}" class="img-responsive" alt="${image.title}">
+                 </a>
+              </div>
+           </div>`;
+        galleryContainer.appendChild(item);
+     });
+    
+     imagesO.forEach(image => {
+        const item = document.createElement('div');
+        item.className = 'col-sm-6 col-md-3 col-lg-3 Oleo';
+        item.innerHTML = `
+           <div class="portfolio-item">
+              <div class="hover-bg">
+                 <a href="${image.src}" title="${image.title}" data-description="${image.description}" data-lightbox-gallery="gallery1">
+                    <div class="hover-text">
+                       <h4>${image.title}</h4>
+                    </div>
+                    <img src="${image.src}" class="img-responsive" alt="${image.title}">
+                 </a>
+              </div>
+           </div>`;
+        galleryContainer.appendChild(item);
+     });
+    
+    imagesCarb.forEach(image => {
+        const item = document.createElement('div');
+        item.className = 'col-sm-6 col-md-3 col-lg-3 carboncillo';
+        item.innerHTML = `
+           <div class="portfolio-item">
+              <div class="hover-bg">
+                 <a href="${image.src}" title="${image.title}" data-description="${image.description}" data-lightbox-gallery="gallery1">
+                    <div class="hover-text">
+                       <h4>${image.title}</h4>
+                    </div>
+                    <img src="${image.src}" class="img-responsive" alt="${image.title}">
+                 </a>
+              </div>
+           </div>`;
+        galleryContainer.appendChild(item);
+     });
+ 
+ }
+ 
+ generateGallery();
